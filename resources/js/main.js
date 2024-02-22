3a7e7e0420775da3821c75af46a4327b20d759fe
Sibin <sibin.limenzy@gmail.com>
2024-02-22 20:50:10 +0530

    changes 1

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,1 +1,17 @@
 new CircleType(document.getElementById('demo3'));
+
+$(document).ready(function() {
+    fetch('resources/json/main.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+});
